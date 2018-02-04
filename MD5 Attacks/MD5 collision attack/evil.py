b3b4c162871047c36b76116d920a8ef1b672f9ae
Gunj Manseta <gunjmanseta@gmail.com>
2018-02-04 14:45:42 -0700

    Adding MD5 attacks

---
+++ MD5 Attacks/MD5 collision attack/evil.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# 64 chars
+blob = """
+€B¥#`„XCâ‚ˆì€ÿ€ßB‚ë›:æyòÎ&Ÿõã°áj(}Ãjn2›Uù·Qiwú4ÍjPj¨&İR˜V7ÿÉ(êĞylº]ˆÂZHáõ!)9Ş„™ãË_ëA¡ÜÈQr,¤ë˜­°_1+˜KOnµà©n²j'V"""
+if blob == """
+€B¥#`„XCâ‚ˆì€ÿ€ßB‚k›:æyòÎ&Ÿõã°áj(}Ãjn2›•Tù·Qiwú4ÍjPj¨¦İR˜V7ÿÉ(êĞylº]ˆÂZHáõ!”)9Ş„™ãË_ëA¡ÜÈQr,¤ë˜­0`1+˜KOnµà©n2j'V""":
+	print "I come in peace."
+else:
+	print "Prepare to be destroyed!"
---
+++ MD5 Attacks/MD5 collision attack/good.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# 64 chars
+blob = """
+€B¥#`„XCâ‚ˆì€ÿ€ßB‚k›:æyòÎ&Ÿõã°áj(}Ãjn2›•Tù·Qiwú4ÍjPj¨¦İR˜V7ÿÉ(êĞylº]ˆÂZHáõ!”)9Ş„™ãË_ëA¡ÜÈQr,¤ë˜­0`1+˜KOnµà©n2j'V"""
+if blob == """
+€B¥#`„XCâ‚ˆì€ÿ€ßB‚k›:æyòÎ&Ÿõã°áj(}Ãjn2›•Tù·Qiwú4ÍjPj¨¦İR˜V7ÿÉ(êĞylº]ˆÂZHáõ!”)9Ş„™ãË_ëA¡ÜÈQr,¤ë˜­0`1+˜KOnµà©n2j'V""":
+	print "I come in peace."
+else:
+	print "Prepare to be destroyed!"
